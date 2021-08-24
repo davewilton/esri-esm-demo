232e83bfe20cd717cd16dcd7d2499096c029809d
David Wilton <dave_wilton@msn.com>
2021-08-24 13:29:05 +1000

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import "./styles.css";
+import { MapView } from "./Map";
+
+export default function App() {
+  return (
+    <div className="App">
+      <MapView></MapView>
+    </div>
+  );
+}
---
+++ src/esri/map.ts
@@ -0,0 +1,21 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+
+import MapView from "@arcgis/core/views/MapView";
+import Map from "@arcgis/core/Map";
+import esriConfig from "@arcgis/core/config";
+
+export function initializeMap(ref: HTMLDivElement) {
+  // create from a web map?
+  const map = new Map({
+    basemap: "arcgis-topographic" // Basemap layer service
+  });
+
+  const view = new MapView({
+    container: ref,
+    map: map,
+    center: [-118.805, 34.027], // Longitude, latitude
+    zoom: 13 // Zoom level
+  });
+
+  return view;
+}
